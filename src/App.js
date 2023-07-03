b91388d261848ca086a5f1349df743da2a9dfbd2
Neerab <89482636+n-pathipaka@users.noreply.github.com>
2023-07-03 17:17:53 -0500

    Added some code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -10,16 +9,10 @@ import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
+
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
-
 	return (
 		<div className="App">
 			<Routes>
