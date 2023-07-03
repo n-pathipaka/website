9b3f7487daadb71e8ecebecdcbb5cef13fda5f3f
Neerab <89482636+n-pathipaka@users.noreply.github.com>
2023-07-03 15:21:12 -0500

    Pushing the code

---
+++ src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "Personal Portifolio",
+		name: "Neerab P.",
+		email: "neerab007@gmail.com, nefi2237@colorado.edu",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/n-pathipaka/",
+		linkedin: "https://www.linkedin.com/in/neerab/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Software Engineer and AI Enthusiast.",
+		description:
+			"I am a Full stack developer with expertise in React,Python and c++. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Neerab. I love training deep neural nets.",
+		description:
+			""//"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Cureently, I am in love with training deep neural nets, Wait for my articles and podcasts for sharing the information.",
+	},
+
+	articles: {
+		title: "I'm am going to share my understanding of fine-tuning articles soon.",
+		description:
+			""//"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Pyhton Compiler",
+			description:
+				" Compiler for user-friendly functions to draw graphics from Python to WebAssembly.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/n-pathipaka/project",
+		},
+		{
+			title: "PureNav",
+			description:
+				"User-Friendly Navigation App: Features with upcoming trip alerts, scheduling, real-time road quality, and air quality updates.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/n-pathipaka/PureNav",
+		},
+		{
+			title: "AI Fitness Trainer",
+			description:
+				"Real-time posture correctness feedback for workout videos.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/n-pathipaka/AI_FITNESS_TRAINER",
+		},
+
+		{
+			title: "Edge Computing",
+			description:
+				"MEC-based video caching with dynamic transcoding for optimized streaming",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Activity Recommender",
+			description:
+				"Recommendation system for Acitivites to do at a place and a time.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	
+	],
+};
+
+export default INFO;
